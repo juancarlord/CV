ebcee922ee944da233a4efc038965cd75c5069e8
juancarlord <juancagomezmendez@gmail.com>
2020-04-30 10:27:32 -0500

    Hoja de vida

---
+++ js/queries.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(".info").click(function () {
+    $(".panel-info").slideToggle();
+  });
+});
+$(document).ready(function () {
+  $(".skills").click(function () {
+    $(".panel-skills").slideToggle();
+  });
+});
+$(document).ready(function () {
+  $(".contact").click(function () {
+    $(".panel-contact").slideToggle();
+  });
+});
